186ec4950fff8eb9869762abc7814f4e55b217f1
HARSHITHA <harshithaduddola2003@gmail.com>
2025-09-13 18:44:34 -0400

    add datatype.py

---
+++ datatype.py
@@ -0,0 +1,18 @@
+
+a_list = ["2", 3, 7.5, None]
+tup= ('A', 'B', 'C', 4)
+b_list= list(tup)
+type(a_list)
+
+Tup1 = 4,5
+Tup2 = (23,'abc',4.56,(2,3))
+Tup3 = (1,)
+Tup4 = (1)
+Tup5 = 1,
+Tup6 = tuple('string')
+type(Tup1)
+type(Tup2)
+type(Tup3)
+type(Tup4)
+type(Tup5)
+type(Tup6)
